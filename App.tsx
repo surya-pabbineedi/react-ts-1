b79714710d4b7720e678a3640381b8f27590ceb5
surya-pabbineedi <98421392+surya-pabbineedi@users.noreply.github.com>
2022-11-07 12:16:56 +0530

    2 files modified

--- App.tsx
+++ App.tsx
@@ -5,29 +5,15 @@ import Todos from './Todos';
 
 export default function App() {
   const [count, setCount] = useState(0);
-  const [todos, setTodos] = useState([]);
 
   const increment = () => {
     setCount((c) => c + 1);
   };
 
-  const addTodo = () => {
-    setTodos((t) => [...t, 'New Todo']);
-  };
-
   return (
     <div>
-      <em>
-        Upon clicking the counter action Todos component re-renders. Optimize
-        the code so that Todos components re-renders only if todos prop has been
-        changed.
-      </em>
-      <Todos todos={todos} addTodo={addTodo} />
+      <Todos />
       <hr />
-      <div>
-        Count: {count}
-        <button onClick={increment}>+</button>
-      </div>
     </div>
   );
 }
--- Todos.tsx
+++ Todos.tsx
@@ -1,16 +1,54 @@
 import * as React from 'react';
+import { useReducer } from 'react';
 
-const Todos = ({ todos, addTodo }) => {
-  console.log('Todos rendered');
+const initialTodos = [
+  {
+    id: 1,
+    title: 'Todo 1',
+    complete: false,
+  },
+  {
+    id: 2,
+    title: 'Todo 2',
+    complete: false,
+  },
+];
+
+const todoReducer = (todos, action) => {
+  switch (action.type) {
+    case 'COMPLETE': {
+      const todo = todos.find((todo) => todo.id === action.id);
+      todo.complete = !todo.complete;
+    }
+    default:
+  }
+
+  return todos;
+};
+
+function Todos() {
+  const [todos, dispatch] = useReducer(todoReducer, initialTodos);
+
+  const handleComplete = (todo) => {
+    dispatch({ type: 'COMPLETE', id: todo.id });
+  };
+
   return (
     <div>
-      <h2>My Todos</h2>
-      {todos.map((todo, index) => {
-        return <p key={index}>{todo}</p>;
-      })}
-      <button onClick={addTodo}>Add Todo</button>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <label>
+            <input
+              type="checkbox"
+              checked={todo.complete}
+              onChange={() => handleComplete(todo)}
+            />
+            {todo.title}
+          </label>
+        </div>
+      ))}
     </div>
   );
-};
+}
 
 export default Todos;
