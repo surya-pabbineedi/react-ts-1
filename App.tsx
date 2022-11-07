9e943a63d9e68bc6bda2f9b10e15182f7847298a
surya-pabbineedi <98421392+surya-pabbineedi@users.noreply.github.com>
2022-11-07 11:52:46 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -2,10 +2,18 @@ import * as React from 'react';
 import './style.css';
 
 export default function App() {
+  const [count, setCount] = React.useState(0);
+
+  const updateCount = () => {
+    setCount((count) => count++);
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Why the counter is not incrementing?</h1>
+      <button onClick={() => updateCount()}>
+        I count number of clicks on me - {count}
+      </button>
     </div>
   );
 }
