8be088a777fb6c4c5156217d447fe76f157b31e3
surya-pabbineedi <98421392+surya-pabbineedi@users.noreply.github.com>
2022-11-08 09:16:02 +0530

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -10,10 +10,26 @@ export default function App() {
 
   return (
     <div>
-      <h1>Why the counter is not incrementing?</h1>
-      <button onClick={() => updateCount()}>
-        I count number of clicks on me - {count}
-      </button>
+      <h1>Hello Aspirant!</h1>
+      <p>
+        Develop react components to retrive user posts and group them by userId
+        and display title and body. All the posts should be grouped visually by
+        user first & last name
+      </p>
+      <p>
+        posts:
+        <a href="https://jsonplaceholder.typicode.com/posts">
+          https://jsonplaceholder.typicode.com/posts
+        </a>
+      </p>
+      <p>
+        users:
+        <a href="https://jsonplaceholder.typicode.com/users">
+          https://jsonplaceholder.typicode.com/users
+        </a>
+      </p>
+
+      <Dummy />
     </div>
   );
 }
