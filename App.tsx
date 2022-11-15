345757e46bf6fe5da5e53cca3f48ecd9b5aedb49
surya-pabbineedi <98421392+surya-pabbineedi@users.noreply.github.com>
2022-11-15 15:46:26 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,15 +1,8 @@
 import * as React from 'react';
-import { useState } from 'react';
 import './style.css';
 import Todos from './Todos';
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount((c) => c + 1);
-  };
-
   return (
     <div>
       <Todos />
